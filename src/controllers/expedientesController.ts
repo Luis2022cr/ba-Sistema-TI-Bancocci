b201cf9d743b5bf8c1c1ffa2bfd8b550f35e3b76
JimmyArdon <123607114+JimmyArdon@users.noreply.github.com>
2025-01-09 14:13:21 -0600

    UpdateExpedienteBE (#17)

--- src/controllers/expedientesController.ts
+++ src/controllers/expedientesController.ts
@@ -4,8 +4,8 @@ import pool from '../database/mysql';
 
 export const getExpediente = async (req: Request, res: Response): Promise<void> => {
     try {
-        const [expediente_prestamos]: any = await pool.query(`SELECT 
-    ep.id, ep.numero_cliente, ep.nombre_cliente,  ep.estado_id,  ep.agencia_id, ep.estante, 
+        const [expediente_prestamos]: any = await pool.query(` select
+ ep.id, ep.numero_cliente, ep.nombre_cliente,  ep.estado_id,  ep.agencia_id, ep.estante, 
     ep.columna,  ep.fila, ep.comentarios, ep.fecha_entrada, ep.fecha_salida, 
     ep.usuario_id, ep.responsable,
     esp.nombre  as estado ,
@@ -16,6 +16,7 @@ FROM
 join estado_prestamos esp on ep.estado_id = esp.id 
 join agencias a on ep.agencia_id  = a.id 
 join usuario u  on ep.usuario_id  = u.id 
+Where ep.estado_id = 1
 `);
         res.status(200).json(expediente_prestamos);
     } catch (error) {
@@ -25,7 +26,7 @@ join usuario u  on ep.usuario_id  = u.id
 
 export const getExpedientePorNumeroCliente = async (req: Request, res: Response): Promise<void> => {
     try {
-        const { id } = req.params;
+        const { clienteId } = req.params;
         const [expediente_prestamos]: any = await pool.query(`
             SELECT 
                 ep.id, ep.numero_cliente, ep.nombre_cliente,  ep.agencia_id, ep.estante, 
@@ -38,6 +39,35 @@ export const getExpedientePorNumeroCliente = async (req: Request, res: Response)
             join agencias a on ep.agencia_id  = a.id 
             join usuario u  on ep.usuario_id  = u.id 
             where ep.numero_cliente = ?
+        `, [clienteId]);
+
+        if (expediente_prestamos.length > 0) {
+            res.status(200).json(expediente_prestamos[0]);
+        } else {
+            res.status(404).json({ error: 'expediente no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener el expediente' });
+    }
+};
+
+
+export const getExpedientePorId = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const [expediente_prestamos]: any = await pool.query(`
+            SELECT 
+                ep.id, ep.numero_cliente, ep.nombre_cliente, ep.estado_id, ep.agencia_id, ep.estante, 
+                ep.columna, ep.fila, ep.comentarios, 
+                ep.responsable,
+                a.nombre as agencia, 
+                esp.nombre as estado
+            FROM 
+                expediente_prestamos ep
+            join agencias a on ep.agencia_id  = a.id 
+            join estado_prestamos esp  on ep.estado_id  = esp.id 
+            where ep.id = ?
+
         `, [id]);
 
         if (expediente_prestamos.length > 0) {
@@ -50,24 +80,24 @@ export const getExpedientePorNumeroCliente = async (req: Request, res: Response)
     }
 };
 
-/*
+
 export const getExpedientesDeBaja = async (req: Request, res: Response): Promise<void> => {
     try {
         // Base query para filtrar por estado
         const query = `
-            SELECT 
-                i.id, i.codigo, i.serie, i.comentarios, i.fecha_creacion, i.fecha_modificacion, 
-                ti.nombre AS tipo_inventario, m.nombre AS marca, md.nombre as modelo, ag_origen.nombre AS agencia_origen, 
-                ag_actual.nombre AS agencia_actual, ag_origen.codigo AS codigo_agencia_origen, est.nombre AS estado, u.nombre AS usuario
-            FROM inventario i
-            JOIN tipo_inventario ti ON i.tipo_inventario_id = ti.id
-            JOIN marca m ON i.marca_id = m.id
-            JOIN modelo md ON i.modelo_id = md.id
-            JOIN agencias ag_origen ON i.agencias_id_origen = ag_origen.id
-            JOIN agencias ag_actual ON i.agencias_id_actual = ag_actual.id
-            JOIN estado est ON i.estado_id = est.id
-            JOIN usuario u ON i.usuario_id = u.id
-            WHERE i.estado_id = 4
+            select
+ ep.id, ep.numero_cliente, ep.nombre_cliente,  ep.estado_id,  ep.agencia_id, ep.estante, 
+    ep.columna,  ep.fila, ep.comentarios, ep.fecha_entrada, ep.fecha_salida, 
+    ep.usuario_id, ep.responsable,
+    esp.nombre  as estado ,
+    a.nombre as agencia,
+    u.nombre as usuario
+FROM 
+    expediente_prestamos ep
+join estado_prestamos esp on ep.estado_id = esp.id 
+join agencias a on ep.agencia_id  = a.id 
+join usuario u  on ep.usuario_id  = u.id 
+Where ep.estado_id = 2
         `;
 
         // Ejecutamos la consulta con el estado_id como parámetro
@@ -79,8 +109,6 @@ export const getExpedientesDeBaja = async (req: Request, res: Response): Promise
         res.status(500).json({ error: 'Error al obtener los expedientes por estado' });
     }
 };
-
-*/
 
 export const crearExpediente = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -116,7 +144,7 @@ export const crearExpediente = async (req: Request, res: Response): Promise<void
             return;
         }
 
-        console.log(req.body, userId)
+        
         // Crear el inventario
         await pool.query(`
             INSERT INTO expediente_prestamos (numero_cliente, nombre_cliente, estado_id, agencia_id, estante,
@@ -139,5 +167,117 @@ export const crearExpediente = async (req: Request, res: Response): Promise<void
         res.status(201).json({ message: 'Expediente creado exitosamente' });
     } catch (error) {
         res.status(500).json({ error: 'Error al crear el Expediente' });
+    }
+};
+
+export const actualizarExpediente = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const userId = (req as any).user?.id; // ID del usuario autenticado
+        const { id } = req.params;
+        const { numero_cliente, nombre_cliente, estado_id, agencia_id, estante,
+            columna, fila, comentarios, responsable } = req.body;
+
+        // Validar que todos los campos estén presentes
+        if (!numero_cliente || !nombre_cliente || !estado_id || !agencia_id || !estante || !columna
+             || !fila || !comentarios || !responsable) {
+            res.status(400).json({ error: 'Todos los campos son obligatorios' });
+            return;
+        }
+
+        // Obtener el expediente actual antes de actualizar
+        const [expedienteActual]: any = await pool.query(`
+            SELECT numero_cliente, nombre_cliente, estado_id, agencia_id, estante,
+            columna, fila, comentarios, responsable
+            FROM expediente_prestamos
+            WHERE numero_cliente = ?
+        `, [numero_cliente]);
+
+        
+        if (expedienteActual.length === 0) {
+            res.status(404).json({ error: 'Expediente no encontrado' });
+            return;
+        }
+
+        const expedienteAnterior = expedienteActual[0];
+
+        // Validar que el numero_cliente no esté siendo cambiado a uno que ya existe
+        if (numero_cliente !== expedienteAnterior.numero_cliente) {
+            const [expedienteExistente]: any = await pool.query(
+                'SELECT id FROM expediente_prestamos WHERE numero_cliente = ? AND id != ?',
+                [numero_cliente, id]
+            );
+        
+            if (expedienteExistente.length > 0) {
+                res.status(400).json({ error: 'El número de cliente ya está registrado en otro expediente' });
+                return;
+            }
+        }
+
+        // Obtener nombres descriptivos para el expediente anterior y el nuevo
+        const obtenerNombre = async (tabla: string, id: number) => {
+            if (!id) return null;
+            const [resultado]: any = await pool.query(`SELECT nombre FROM ${tabla} WHERE id = ?`, [id]);
+            return resultado.length > 0 ? resultado[0].nombre : null;
+        };
+
+        const nombresAnterior = {
+            estado: await obtenerNombre('estado_prestamos', expedienteAnterior.estado_id),
+            agencia: await obtenerNombre('agencias', expedienteAnterior.agencia_id),
+        };
+
+        const nombresNuevo = {
+            estado: await obtenerNombre('estado_prestamos', estado_id),
+            agencia: await obtenerNombre('agencias', agencia_id),
+        };
+
+        // Actualizar el expediente
+        const [result]: any = await pool.query(`
+            UPDATE expediente_prestamos
+            SET numero_cliente = ?, nombre_cliente = ?, estado_id = ?, agencia_id = ?, estante = ?,
+            columna = ?, fila = ?, comentarios = ?, responsable = ?
+            WHERE id = ?
+        `, [numero_cliente, nombre_cliente, estado_id, agencia_id, estante,
+            columna, fila, comentarios, responsable,
+           userId, id]);
+
+        if (result.affectedRows > 0) {
+            // Comparar los campos modificados y generar el log de cambios
+            let cambios: string[] = [];
+
+            // Función para registrar cambios
+            const registrarCambio = (campo: string, valorAnterior: any, valorNuevo: any) => {
+                if (valorAnterior !== valorNuevo) {
+                    cambios.push(`${campo}: '${valorAnterior}' -> '${valorNuevo}'`);
+                }
+            };
+
+            registrarCambio('Número Cliente', expedienteAnterior.numero_cliente, numero_cliente);
+            registrarCambio('Nombre Cliente', expedienteAnterior.nombre_cliente, nombre_cliente);
+            registrarCambio('Estado Prestamo', nombresAnterior.estado, nombresNuevo.estado);
+            registrarCambio('Agencia', nombresAnterior.agencia, nombresNuevo.agencia);
+            registrarCambio('Estante', expedienteAnterior.estante, estante);
+            registrarCambio('Columna', expedienteAnterior.columna, columna);
+            registrarCambio('Fila', expedienteAnterior.fila, fila);
+            registrarCambio('Comentarios', expedienteAnterior.comentarios, comentarios);
+            registrarCambio('Responsable', expedienteAnterior.responsable, responsable);
+
+            // Generar la descripción y el cambio realizado para el log
+            const descripcion = `Se actualizó el expediente con código: ${expedienteAnterior.numero_cliente}.`;
+            const cambioRealizado = cambios.length > 0 ? cambios.join(', ') : 'Sin cambios detectados';
+
+            // Registrar el log si hubo cambios
+            if (cambios.length > 0) {
+                await pool.query(`
+                    INSERT INTO logs (descripcion, cambio_realizado, usuario_id)
+                    VALUES (?, ?, ?)
+                `, [descripcion, cambioRealizado, userId]);
+            }
+
+            res.status(200).json({ message: 'Expediente actualizado exitosamente' });
+        } else {
+            res.status(404).json({ error: 'Expediente no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error al actualizar el expediente' });
     }
 };
